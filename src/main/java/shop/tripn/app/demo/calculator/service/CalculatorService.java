fe7c613e43874890a59ca47c4091ca5e7536c2d5
Younyeojin <yjyoon2107@gmail.com>
2021-08-10 17:48:33 +0900

    project

---
+++ src/main/java/shop/tripn/app/demo/calculator/service/CalculatorService.java
@@ -0,0 +1,7 @@
+package shop.tripn.app.demo.calculator.service;
+
+import shop.tripn.app.demo.calculator.entity.Calculator;
+
+public interface CalculatorService {
+    int createRandomNumbers();
+}
